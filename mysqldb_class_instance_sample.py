176a654c1b7cf6e54da5a061a3ea72cbf485697e
Jan Beilicke <dev@jotbe-fx.de>
2011-04-02 02:06:22 +0200

    Added sample MySQL database connection (class-based)

---
+++ mysqldb_class_instance_sample.py
@@ -0,0 +1,54 @@
+#!/bin/env python
+
+"""Sample instantiation of db class
+
+This script is just a sample for dealing with a database in Python
+
+== Prerequisites ==
+
+The following commands are executed using the MySQL console.
+
+- Create a new database 'pytest':
+mysql> CREATE DATABASE pytest;
+
+- Create a test user 'pyuser':
+mysql> CREATE USER 'pyuser'@'localhost' IDENTIFIED by 'pyt3ster';
+
+- Grant rights to user:
+mysql> GRANT ALL ON pytest.* TO 'pyuser'@'localhost';
+
+For creating and populating the demo table, one should run `mysql-sample`.
+
+TODO: Put the connect() code into a new class or maybe into GeneralDBCmd (refactor name!)
+
+"""
+__author__ = 'Jan Beilicke <dev@jotbe-fx.de>'
+__date__ = '2011-04-01'
+
+import MySQLdb as mdb
+from mysqldb_class_sample import GeneralDBCmd
+
+if __name__ == '__main__':
+    print '__name__: ', __name__
+    
+    dbhost = '127.0.0.1'
+    dbuser = 'pyuser'
+    dbpwd = 'pyt3ster'
+    db = 'pytest'
+    tbl = 'tbl_test'
+
+    c = mdb.connect(dbhost, dbuser, dbpwd, db)
+
+    q = GeneralDBCmd(c, tbl)
+    q.debug = True
+    
+    print dir(q)
+    print 'MySQL-Version: %s' % q.get_mysql_version()
+
+    print 'Querying field 1 of row 1 \'q[1][1]\': ', q[1][1]
+
+    for x in xrange(0,2):
+        print 'Name (range) #%s: %s' % (x, q[x][1])
+    
+    for x in q:
+        print 'Name: %s' % (x[1],)
---
+++ mysqldb_class_sample.py
@@ -0,0 +1,51 @@
+#!/bin/env python
+
+"""Sample database class for MySQL
+
+This script is just a sample for dealing with a database class
+
+"""
+__author__ = 'Jan Beilicke <dev@jotbe-fx.de>'
+__date__ = '2011-04-01'
+
+import MySQLdb as mdb
+
+class GeneralDBCmd:
+    
+    """ Provide general database commands. """
+    
+    def __init__(self, dbcon, tbl):
+        self.dbcon = dbcon
+        self.dbcur = self.dbcon.cursor()
+        self.tbl = tbl
+        self.debug = True
+    def __getitem__(self, item):
+        self.dbcur.execute('''
+            SELECT * FROM %s LIMIT %s, 1;
+        ''' % (self.tbl, item))
+        return self.dbcur.fetchone()
+    def _query(self, q):
+        """ Display the query in debug mode and execute it. """
+        if self.debug: print 'Query: %s' % (q)
+        self.dbcur.execute(q)
+    def __iter__(self):
+        """ Create a data set and return an iterator (self). """
+        q = """
+            SELECT * FROM %s
+        """ % (self.tbl)
+        self._query(q)
+        # return iterator object with 'next()' method
+        return self
+    def next(self):
+        """ Return the next item in dataset or tell Python to stop. """
+        r = self.dbcur.fetchone()
+        if not r:
+            raise StopIteration
+        return r
+    def get_mysql_version(self):
+        """Return the MySQL version of the server"""
+        q = """
+            SELECT VERSION();
+        """
+        self._query(q)
+        return self.dbcur.fetchone()
